c62659cfedb776943902d8ec5445022a3293e503
Nils Rydh <nils.rydh@gmail.com>
2013-08-28 11:51:04 +0200

    added middleware to allow CORS

--- app.js
+++ app.js
@@ -7,35 +7,47 @@ var app = express();
 var server = http.createServer(app);
 var routes = require('./routes');
 
-// Configuration
- 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
 
-  //var jshtml = require('jshtml-express');
-  //app.engine('jshtml', jshtml);
-  app.engine('.html', require('ejs').__express)
-  app.set('view engine', 'html');
-  //app.set('view engine', 'jade');
-  
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+// Configuration
+
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+
+	//var jshtml = require('jshtml-express');
+	//app.engine('jshtml', jshtml);
+	app.engine('.html', require('ejs').__express)
+	app.set('view engine', 'html');
+	//app.set('view engine', 'jade');
+
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(allowCrossDomain);
+	app.use(express.static(__dirname + '/public'));
 });
+
+
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
- 
+
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // routes
 require('./routes/index')(app);
- 
+
 server.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", server.address().port,
-     app.settings.env);
+	console.log("Express server listening on port %d in %s mode", server.address().port,
+		app.settings.env);
 });
