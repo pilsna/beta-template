5d0d6d0a2f1101fa274cd53df2fa205b75d0c7fa
Nils Rydh <nils.rydh@gmail.com>
2013-08-30 07:15:52 +0200

    added a route for esri.html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ module.exports = function(app) {
    app.get('/', function(req, res){
      res.render('index', { title: 'template' })
    });
+   app.get('/esri', function(req, res){
+     res.render('esri', { title: 'template' })
+   });
 };
