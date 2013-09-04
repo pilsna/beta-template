8ef14c68bc94088dcbf37cb94eb96d7b29f580cc
Nils Rydh <nils.rydh@gmail.com>
2013-09-04 14:09:57 +0200

    some oauth and routing stuff

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@
  * GET home page.
  */
 
+var webmaps = {
+	'wind': 'a30160193b1f4922be09d62940f65696',
+	'andet': 'asdf'
+};
+
 module.exports = function(app) {
    app.get('/', function(req, res){
      res.render('index', { title: 'template' })
@@ -11,5 +16,11 @@ module.exports = function(app) {
    });
    app.get('/simple', function(req, res){
      res.render('simple/simple', { title: 'template' })
+   });
+   app.get('/simple/:template', function(req, res){
+     res.render('simple/' + req.params.template, { title: 'template' })
+   });
+   app.get('/simple/:template/:map', function(req, res){
+     res.redirect('simple/' + req.params.template + '?webmap=' + webmaps[req.params.map])
    });
 };
