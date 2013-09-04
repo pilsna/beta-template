35303d88104dba4d0558911be74988e3a11338bb
Nils Rydh <nils.rydh@gmail.com>
2013-09-04 17:06:23 +0200

    added esri boilerplate for ago templates

--- public/js/routing.js
+++ public/js/routing.js
@@ -1,23 +1,42 @@
-require(["esri/map", "esri/arcgis/utils", "esri/IdentityManager",  
-    "dojo/on", "dojo/dom", "dojo/domReady!"], 
-    loadWebmap
 
+console.log(dojoConfig.packages);
+require(["esri/map", "esri/arcgis/utils", "esri/arcgis/Portal", "oauth/OAuthHelper",  
+	"dojo/on", "dojo/domReady!"], 
+	initialize
 );
 
+function initialize(map, utils, portal, oauthHelper, on, domReady){
+	loadWebmap();
 
-function loadWebmap(map, utils, identityManager, on, dom, domReady){
-	var webmap;
-	var urlObject = esri.urlToObject(document.location.href);
-	if (urlObject.query === null) {
-		loadMap();
-	} else {
-		webmap = urlObject.query.webmap;	
-		esri.arcgis.utils.createMap(webmap,"mapcontainer",{
-		   mapOptions:{
-		     slider:false
-		   }
-		}).then(function(response){
-		    map = response.map;
-		});
-	}
+	oAuthHelper.init({
+		appId:      "bAkrQgFPquOr8OXa",
+		portal:     "http://www.arcgis.com",
+        expiration: (14 * 24 * 60), // 2 weeks, in minutes
+        popup:      true
+    });
+
+
+	if (!oAuthHelper.isSignedIn()) {
+	    // Anonymous view
+        domStyle.set("anonymousPanel", "display", "block");
+        domStyle.set("personalizedPanel", "display", "none");
+    	oAuthHelper.signIn(); //.then(displayItems);
+    }
+}
+
+function loadWebmap(){
+    var webmap;
+    var urlObject = esri.urlToObject(document.location.href);
+    if (urlObject.query === null) {
+    	loadMap();
+    } else {
+    	webmap = urlObject.query.webmap;	
+    	esri.arcgis.utils.createMap(webmap,"mapcontainer",{
+    		mapOptions:{
+    			slider:false
+    		}
+    	}).then(function(response){
+    		map = response.map;
+    	});
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ module.exports = function(app) {
    app.get('/', function(req, res){
      res.render('index', { title: 'template' })
    });
+   app.get('/route', function(req, res){
+     res.render('route', { title: 'template' })
+   });
    app.get('/esri', function(req, res){
      res.render('esri', { title: 'template' })
    });
